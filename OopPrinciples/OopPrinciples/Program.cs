8999e324b0f91e96185366c64c935dfc07414806
nikita <nikita.yazkov@yandex.ru>
2024-04-16 21:44:59 +0300

    Создано приложение для тестирования принципов ООП

---
+++ OopPrinciples/OopPrinciples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OopPrinciples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
